4fa4e49796d9a662f4da555f7ab091214a4ae7b7
Duncan Sykes <sykesduncan32@gmail.com>
2020-12-01 10:21:46 +1100

    Codebase (feature) : 1112 : Added camera follow functionality on Y Axis

---
+++ project/Assets/Scripts/Player/camerafollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    [SerializeField] private Transform followObject;
+    [SerializeField] private Vector3 offset;
+
+    private Vector3 newPosition = new Vector3();
+   
+    private Camera main_camera;
+
+    
+    public void InitCamera(Camera camera){
+        main_camera = camera;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        
+        newPosition.x = 0;
+        newPosition.z = offset.z;
+        newPosition.y = followObject.position.y;
+
+        main_camera.gameObject.transform.position = newPosition;
+    }
+    
+        
+    
+}
--- project/Assets/Scripts/Player/playerController.cs
+++ project/Assets/Scripts/Player/playerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    [SerializeField] private Camera playerCamera;
+    
+    [SerializeField] private camerafollow cameraController;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraController.InitCamera(playerCamera);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+
     }
 }
