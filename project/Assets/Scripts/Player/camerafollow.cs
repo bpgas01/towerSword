6d74c0d67e3531768cab5428d9d5bc046f7d7a2e
Duncan Sykes <sykesduncan32@gmail.com>
2020-12-01 11:34:57 +1100

    Documentation (TDD) : 1113 : Created inital commit for TDD.

--- project/Assets/Scripts/Player/camerafollow.cs
+++ project/Assets/Scripts/Player/camerafollow.cs
@@ -18,7 +18,7 @@ public class camerafollow : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void FixedUpdate()
+    private void Update()
     {
         
         newPosition.x = 0;
--- project/Assets/Scripts/Player/playerController.cs
+++ project/Assets/Scripts/Player/playerController.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    [Header("Camera Settings")]
     [SerializeField] private Camera playerCamera;
-    
     [SerializeField] private camerafollow cameraController;
+
+    [Header("Player Settings")]
+    [SerializeField] private Transform playerObjectTransform;
+    [SerializeField] private float playerSpeed;
+
+     float yPos = 0 ;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +22,32 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateMovement();
+
 
 
     }
+
+
+
+    void UpdateMovement(){
+
+        float xPos = 0;
+       
+
+        yPos += 1;
+
+        playerObjectTransform.position = new Vector3(0,yPos * playerSpeed * Time.deltaTime,0);
+
+        if (Input.GetButton("Jump")){
+            xPos += 1;
+            playerObjectTransform.position += new Vector3(xPos * playerSpeed * Time.deltaTime,0,0);
+
+        }
+
+        
+
+    }
+
+
 }
